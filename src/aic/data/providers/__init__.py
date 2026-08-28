2d53b06b71ee6b4f2543c59c6252c6a3f3738cf6
Maya Buto <butomaya12@gmail.com>
2026-08-29 00:19:08 +0500

    B3.4B: export bounded provider clients

---
+++ src/aic/data/providers/__init__.py
@@ -0,0 +1,41 @@
+from .alpaca_news import (
+    ALPACA_NEWS_ENDPOINT,
+    ALPACA_NEWS_NORMALIZATION_VERSION,
+    AlpacaNewsArticle,
+    AlpacaNewsRead,
+    AlpacaNewsReadError,
+    AlpacaNewsTransport,
+    StdlibAlpacaNewsTransport,
+    load_alpaca_market_data_credentials,
+    read_alpaca_news_window,
+)
+from .sec_filings import (
+    SEC_SECTION_NORMALIZATION_VERSION,
+    SecFilingReadError,
+    SecFilingSection,
+    SecFilingSectionRead,
+    SecHttpTransport,
+    StdlibSecHttpTransport,
+    load_sec_user_agent,
+    read_sec_filing_sections,
+)
+
+__all__ = [
+    "ALPACA_NEWS_ENDPOINT",
+    "ALPACA_NEWS_NORMALIZATION_VERSION",
+    "AlpacaNewsArticle",
+    "AlpacaNewsRead",
+    "AlpacaNewsReadError",
+    "AlpacaNewsTransport",
+    "SEC_SECTION_NORMALIZATION_VERSION",
+    "SecFilingReadError",
+    "SecFilingSection",
+    "SecFilingSectionRead",
+    "SecHttpTransport",
+    "StdlibAlpacaNewsTransport",
+    "StdlibSecHttpTransport",
+    "load_alpaca_market_data_credentials",
+    "load_sec_user_agent",
+    "read_alpaca_news_window",
+    "read_sec_filing_sections",
+]
